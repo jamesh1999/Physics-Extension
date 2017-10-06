e6047e011a51b41ee41391c5ad4f711b049366a0
James Hogge <jameshogge@gmail.com>
2017-10-06 23:55:12 +0100

    Runge-Kutta Method For Field Lines

--- electric_fields.py
+++ electric_fields.py
@@ -16,6 +16,10 @@ DIR_LEN = 16
 MAX_SIZE = 50
 #Field line plot
 SHOW_FIELD_LINES = True
+USE_RUNGE_KUTTA = True
+DELTA = 0.0001
+STEPS = 0
+LINE_COUNT = 12
 
 
 
@@ -34,6 +38,20 @@ def fieldPoint(x, y):
 
 	return tot
 
+#Fourth order runge kutta
+def rungeKutta(x, y, f, delta):
+
+	def norm(v):
+		mag = math.hypot(v[0], v[1])
+		return [v[0] / mag, v[1] / mag]
+
+	k1 = norm(f(x,y))
+	k2 = norm(f(x + k1[0] * delta / 2, y + k1[0] * delta / 2))
+	k3 = norm(f(x + k2[0] * delta / 2, y + k2[0] * delta / 2))
+	k4 = norm(f(x + k3[0] * delta, y + k3[0] * delta))
+
+	return [x + (k1[0] + 2*k2[0] + 2*k3[0] + k4[0]) * delta / 6, y + (k1[1] + 2*k2[1] + 2*k3[1] + k4[1]) * delta / 6]
+
 
 
 pygame.init()
@@ -45,7 +63,7 @@ if SHOW_GRID:
 	data = []
 	for x in range(RESOLUTION):
 		row = []
-		for x in range(RESOLUTION):
+		for y in range(RESOLUTION):
 			row.append(fieldPoint(x/RESOLUTION, y/RESOLUTION))
 		data.append(row)
 
@@ -72,22 +90,50 @@ if SHOW_GRID:
 			pygame.draw.circle(screen, (255,255,255), (int(x/RESOLUTION*WINDOW_SIZE), int(y/RESOLUTION*WINDOW_SIZE)), int(WINDOW_SIZE/RESOLUTION/5), 1)
 
 if SHOW_FIELD_LINES:
-	#Contructs field lines by the Euler method
+	#Contructs field lines numerically
 	for (pos, charge) in CHARGES:
-		for i in range(12):
-			cur = [pos[0] + math.cos(i * math.pi / 6) * 0.02, pos[1] + math.sin(i*math.pi / 6) * 0.02]
+		for i in range(LINE_COUNT):
+			theta = 2 * i * math.pi / LINE_COUNT
+			cur = [pos[0] + math.cos(theta) / RESOLUTION / 3, pos[1] + math.sin(theta) / RESOLUTION / 3]
 
-			for i in range(50):
-				tot = fieldPoint(cur[0], cur[1])
-				mag = math.hypot(tot[0], tot[1])
+			j = 0
+			while STEPS == 0 or j < STEPS:
 
-				if charge < 0:
-					tot[0] *= -1
-					tot[1] *= -1
+				def negPoint(x, y):
+					val = fieldPoint(x, y)
+					return [-val[0], -val[1]]
 
-				ncur = [cur[0] + tot[0] * 0.02 / mag, cur[1] + tot[1] * 0.02 / mag]
+				if USE_RUNGE_KUTTA:
+					ncur = rungeKutta(cur[0], cur[1], fieldPoint if charge > 0 else negPoint, DELTA)
+				else:
+					#Otherwise perform the Euler method
+					tot = fieldPoint(cur[0], cur[1])
+					mag = math.hypot(tot[0], tot[1])
+
+					if charge < 0:
+						tot[0] *= -1
+						tot[1] *= -1
+
+					ncur = [cur[0] + tot[0] * DELTA / mag, cur[1] + tot[1] * DELTA / mag]
+
 				pygame.draw.line(screen, (255,255,255), (cur[0]*WINDOW_SIZE, cur[1]*WINDOW_SIZE), (ncur[0]*WINDOW_SIZE, ncur[1]*WINDOW_SIZE))
+
+				#Refresh screen
+				if not j % 200:
+					pygame.display.flip()
+
+				#Terminate if field line leaves screen or reaches charge
+				if not (0 < ncur[0] < 1 and 0 < ncur[1] < 1): break;
+				terminate = False
+				for (pos1, charge1) in CHARGES:
+					if math.hypot(ncur[0] - pos1[0], ncur[1] - pos1[1]) < 1 / RESOLUTION / 3:
+						terminate = True
+						break;
+				if terminate: break;
+
 				cur = ncur[:]
+
+				j += 1
 
 
 #Display the charges
